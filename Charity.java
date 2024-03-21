455120e9d7680843e71a8edfeec1f964f8dff99d
mattromgg <matthew_romero@greengates.edu.mx>
2024-03-21 08:34:21 -0600

    Gaining access to donation history. Charity and Donor objects.

---
+++ Charity.java
@@ -0,0 +1,11 @@
+class Charity {
+    private String charityName;
+    
+    public Charity(String charityName) {
+        this.charityName = charityName;
+    }
+    
+    public String getCharityName() {
+        return charityName;
+    }
+}
